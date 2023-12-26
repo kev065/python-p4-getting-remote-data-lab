e1e3ddf503badb4b63d5ce7b3162c6c054c4ed70
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-26 17:13:30 +0300

    all tests passing
    repl works by first importing the GetRequester class into the Python interactive shell with the following command: from GetRequester import GetRequester

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,15 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        pass
+    def get_response_body(self):     
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        jsonified = json.loads(self.get_response_body())
+        return jsonified
+
+url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+
+site = GetRequester(url)
+print(site.load_json())
